54d28ec41b9c334e86aafbe219a703edb733ad37
cj3699xiao <354123302@qq.com>
2021-01-05 12:17:16 -0600

    Update MonkeyLearnClient.java

--- src/main/java/com/laioffer/job/external/MonkeyLearnClient.java
+++ src/main/java/com/laioffer/job/external/MonkeyLearnClient.java
@@ -20,7 +20,6 @@ import java.util.*;
 public class MonkeyLearnClient {
     private static final String EXTRACT_URL = Constant.MONKEY_EXTRACT_URL;
     private static final String AUTH_TOKEN = Constant.MONKEY_AUTH_TOKEN;
-    
 
     public List<Set<String>> extract(List<String> articles) {
         ObjectMapper mapper = new ObjectMapper();
